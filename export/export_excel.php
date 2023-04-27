e76e0a9804040714da0d6a1f554a03cc49e5dd02
nisdeg <mongolzkarigun@gmail.com>
2023-04-28 04:14:06 +0800

    xD

--- export/export_excel.php
+++ export/export_excel.php
@@ -19,113 +19,113 @@ $output = "";
 
 ?>
 
-    <table border="1">
+<table border="1">
 
-        <thead>
+    <thead>
+        <tr>
+            <th style="width: 10%">Код</th>
+            <th style="width: 30%">Он/сар/өдөр</th>
+            <th style="width: 19%">Хэрэгсэл</th>
+            <th style="width: 10%; text-align: center">Тоот</th>
+            <th style="width: 15%">Асуудал</th>
+            <th style="width: 30%">Алба Тэнхим</th>
+            <th style="width: 10%; text-align: center">Ажилчин</th>
+            <th style="width: 10%; text-align: center">Төлөв</th>
+            <th style="width: 10%; text-align: center">ҮСИ</th>
+            <th style="width: 10%; text-align: center">СА</th>
+        </tr>
+    </thead>
+    <?php foreach ($data  as $row) {
+
+    ?>
+        <tbody>
+
             <tr>
-                <th style="width: 10%">Код</th>
-                <th style="width: 30%">Он/сар/өдөр</th>
-                <th style="width: 19%">Хэрэгсэл</th>
-                <th style="width: 10%; text-align: center">Тоот</th>
-                <th style="width: 15%">Асуудал</th>
-                <th style="width: 30%">Алба Тэнхим</th>
-                <th style="width: 10%; text-align: center">Ажилчин</th>
-                <th style="width: 10%; text-align: center">Төлөв</th>
-                <th style="width: 10%; text-align: center">ҮСИ</th>
-                <th style="width: 10%; text-align: center">СА</th>
+                <td>
+                    <?php echo $row['order_id'] ?>
+                </td>
+                <td>
+                    <a>
+                        <?php echo $row['orderDate'] ?>
+                    </a>
+                    <br />
+                </td>
+                <td>
+                    <?php echo $row['item'] ?>
+                </td>
+                <td>
+                    <?php echo $row['roomNumber'];
+                    $_SESSION['rNumber'] = $row['roomNumber'];
+                    ?>
+                </td>
+                <td>
+                    <?php echo $row['problem'] ?>
+                </td>
+                <td>
+                    <?php
+                    $useri = $row['userID'];
+                    $sqls = "SELECT * FROM users WHERE user_id  = '$useri'";
+                    $dat = mysqli_query($conn, $sqls);
+                    if (mysqli_num_rows($dat) > 0) {
+                        $usi = mysqli_fetch_assoc($dat);
+                        echo $usi['username'];
+                    }
+                    ?>
+                </td>
+                <td>
+                    <?php
+                    $employee_id = $row['employee_id'];
+                    $emp = "SELECT * FROM employee WHERE employee_id = '$employee_id'";
+                    $res = mysqli_query($conn, $emp);
+                    $rw = mysqli_fetch_assoc($res);
+                    echo $rw['username'];
+                    ?>
+
+                </td>
+
+                <td class="project-actions text-right">
+                    <?php if ($row['dataStatusId'] == 1) {
+                    ?>
+                        <p class="status">Баталсан </p>
+                    <?php
+                    } else {
+                    ?>
+                        <p class="status2">Батлаагүй</p>
+                    <?php
+
+                    };
+                    ?>
+                </td>
+                <td class="project-actions text-right">
+                    <?php if ($row['money_order'] > 0) {
+                    ?>
+                        <p class="status1">Илгээсэн </p>
+                    <?php
+                    } else {
+                    ?>
+                        <p class="status2">Илгээгээгүй</p>
+                    <?php
+
+                    };
+                    ?>
+                </td>
+
+                <td class="project-actions text-right">
+                    <?php if ($row['orderStatus'] == 1) {
+                    ?>
+                        <p class="status">Баталсан </p>
+                    <?php
+                    } else {
+                    ?>
+                        <p class="status2">Батлаагүй</p>
+                    <?php
+                    };
+                    ?>
+                </td>
             </tr>
-        </thead>
-        <?php foreach ($data  as $row) {
+        </tbody>
+    <?php  } ?>
 
-        ?>
-            <tbody>
+</table>
 
-                <tr>
-                    <td>
-                        <?php echo $row['order_id'] ?>
-                    </td>
-                    <td>
-                        <a>
-                            <?php echo $row['orderDate'] ?>
-                        </a>
-                        <br />
-                    </td>
-                    <td>
-                        <?php echo $row['item'] ?>
-                    </td>
-                    <td>
-                        <?php echo $row['roomNumber'];
-                        $_SESSION['rNumber'] = $row['roomNumber'];
-                        ?>
-                    </td>
-                    <td>
-                        <?php echo $row['problem'] ?>
-                    </td>
-                    <td>
-                        <?php
-                        $useri = $row['userID'];
-                        $sqls = "SELECT * FROM users WHERE user_id  = '$useri'";
-                        $dat = mysqli_query($conn, $sqls);
-                        if (mysqli_num_rows($dat) > 0) {
-                            $usi = mysqli_fetch_assoc($dat);
-                            echo $usi['username'];
-                        }
-                        ?>
-                    </td>
-                    <td>
-                        <?php
-                        $employee_id = $row['employee_id'];
-                        $emp = "SELECT * FROM employee WHERE employee_id = '$employee_id'";
-                        $res = mysqli_query($conn, $emp);
-                        $rw = mysqli_fetch_assoc($res);
-                        echo $rw['username'];
-                        ?>
-
-                    </td>
-
-                    <td class="project-actions text-right">
-                        <?php if ($row['dataStatusId'] == 1) {
-                        ?>
-                            <p class="status">Баталсан </p>
-                        <?php
-                        } else {
-                        ?>
-                            <p class="status2">Батлаагүй</p>
-                        <?php
-
-                        };
-                        ?>
-                    </td>
-                    <td class="project-actions text-right">
-                        <?php if ($row['money_order'] > 0) {
-                        ?>
-                            <p class="status1">Илгээсэн </p>
-                        <?php
-                        } else {
-                        ?>
-                            <p class="status2">Илгээгээгүй</p>
-                        <?php
-
-                        };
-                        ?>
-                    </td>
-
-                    <td class="project-actions text-right">
-                        <?php if ($row['orderStatus'] == 1) {
-                        ?>
-                            <p class="status">Баталсан </p>
-                        <?php
-                        } else {
-                        ?>
-                            <p class="status2">Батлаагүй</p>
-                        <?php
-                        };
-                        ?>
-                    </td>
-                </tr>
-            </tbody>
-        <?php  } ?>
-
-    </table>
-
-    <!-- /.card -->
+<!-- /.card -->
